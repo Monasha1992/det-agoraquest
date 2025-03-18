5120e61f7cfe37f49cfc43ecc8cab0f0dad4759e
Prageeth Monasha <prsa1276@student.su.se>
2025-03-18 23:53:23 +0100

    [unity] watch modified;

---
+++ Unity/Agoraquest/Assets/HearIconAnimationScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class HearIconAnimationScript : MonoBehaviour
+{
+    private Transform _transform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+        StartCoroutine(ChangeScaleEverySecond());
+    }
+
+    // Coroutine to change the scale every second
+    private IEnumerator ChangeScaleEverySecond()
+    {
+        var isBig = false;
+        while (true)
+        {
+            _transform.localScale = new Vector3(
+                isBig ? 0.01f : 0.011f,
+                isBig ? 0.01f : 0.011f,
+                isBig ? 0.01f : 0.011f
+            );
+            isBig = !isBig;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
